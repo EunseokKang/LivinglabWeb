4e305671ed13e171d8b9937b55697ee48aa80cf0
EunseokKang <141726520+EunseokKang@users.noreply.github.com>
2024-07-30 12:49:14 +0900

    second  On branch master  Your branch is up to date with 'origin/master'. 	modified:   livinglab/LivinglabWeb/assistance/__pycache__/urls.cpython-311.pyc 	modified:   livinglab/LivinglabWeb/assistance/__pycache__/views.cpython-311.pyc 	new file:   livinglab/LivinglabWeb/assistance/static/img/1.png 	new file:   livinglab/LivinglabWeb/assistance/static/img/2.png 	new file:   livinglab/LivinglabWeb/assistance/static/img/3.png 	new file:   livinglab/LivinglabWeb/assistance/static/img/4.png 	new file:   livinglab/LivinglabWeb/assistance/static/img/5.png 	new file:   livinglab/LivinglabWeb/assistance/templates/assistance/eco/eco-1.html 	new file:   livinglab/LivinglabWeb/assistance/templates/assistance/eco/eco-2.html 	new file:   livinglab/LivinglabWeb/assistance/templates/assistance/eco/eco-3.html 	new file:   livinglab/LivinglabWeb/assistance/templates/assistance/eco/eco-4.html 	new file:   livinglab/LivinglabWeb/assistance/templates/assistance/eco/eco-5.html 	new file:   livinglab/LivinglabWeb/assistance/templates/assistance/eco/eco-6.html 	new file:   livinglab/LivinglabWeb/assistance/templates/assistance/eco/eco-7.html 	new file:   livinglab/LivinglabWeb/assistance/templates/assistance/eco/eco-8.html 	new file:   livinglab/LivinglabWeb/assistance/templates/assistance/eco/eco-9.html 	new file:   livinglab/LivinglabWeb/assistance/templates/assistance/int/int-1.html 	new file:   livinglab/LivinglabWeb/assistance/templates/assistance/int/int-2.html 	new file:   livinglab/LivinglabWeb/assistance/templates/assistance/int/int-3.html 	new file:   livinglab/LivinglabWeb/assistance/templates/assistance/int/int-4.html 	new file:   livinglab/LivinglabWeb/assistance/templates/assistance/int/int-5.html 	new file:   livinglab/LivinglabWeb/assistance/templates/assistance/int/int-6.html 	new file:   livinglab/LivinglabWeb/assistance/templates/assistance/int/int-7.html 	new file:   livinglab/LivinglabWeb/assistance/templates/assistance/law/law-1.html 	new file:   livinglab/LivinglabWeb/assistance/templates/assistance/law/law-2.html 	new file:   livinglab/LivinglabWeb/assistance/templates/assistance/law/law-3.html 	new file:   livinglab/LivinglabWeb/assistance/templates/assistance/law/law-4.html 	new file:   livinglab/LivinglabWeb/assistance/templates/assistance/law/law-5.html 	new file:   livinglab/LivinglabWeb/assistance/templates/assistance/law/law-6.html 	modified:   livinglab/LivinglabWeb/assistance/templates/assistance/mainpage.html 	renamed:    livinglab/LivinglabWeb/assistance/templates/assistance/psy-1.html -> livinglab/LivinglabWeb/assistance/templates/assistance/psy/psy-1.html 	new file:   livinglab/LivinglabWeb/assistance/templates/assistance/psy/psy-2.html 	new file:   livinglab/LivinglabWeb/assistance/templates/assistance/psy/psy-3.html 	modified:   livinglab/LivinglabWeb/assistance/urls.py 	modified:   livinglab/LivinglabWeb/assistance/views.py

--- livinglab/LivinglabWeb/assistance/views.py
+++ livinglab/LivinglabWeb/assistance/views.py
@@ -8,5 +8,76 @@ def mainpage(request):
     return render(request, 'assistance/mainpage.html')
 
 def psy_1(request):
-    # template = loader.get_template('assistance/psy-1.html')
-    return render(request, 'assistance/psy-1.html')
+    return render(request, 'assistance/psy/psy-1.html')
+
+def psy_2(request):
+    return render(request, 'assistance/psy/psy-2.html')
+
+def psy_3(request):
+    return render(request, 'assistance/psy/psy-3.html')
+
+def eco_1(request): 
+    return render(request, 'assistance/eco/eco-1.html')
+
+def eco_2(request):
+    return render(request, 'assistance/eco/eco-2.html')
+
+def eco_3(request):
+    return render(request, 'assistance/eco/eco-3.html')
+
+def eco_4(request):
+    return render(request, 'assistance/eco/eco-4.html')
+
+def eco_5(request):
+    return render(request, 'assistance/eco/eco-5.html')
+
+def eco_6(request):
+    return render(request, 'assistance/eco/eco-6.html')
+
+def eco_7(request):
+    return render(request, 'assistance/eco/eco-7.html')
+
+def eco_8(request):
+    return render(request, 'assistance/eco/eco-8.html')
+
+def eco_9(request):
+    return render(request, 'assistance/eco/eco-9.html')
+
+def law_1(request):
+    return render(request, 'assistance/law/law-1.html')
+
+def law_2(request):
+    return render(request, 'assistance/law/law-2.html')
+
+def law_3(request):
+    return render(request, 'assistance/law/law-3.html')
+
+def law_4(request):
+    return render(request, 'assistance/law/law-4.html')
+
+def law_5(request):
+    return render(request, 'assistance/law/law-5.html')
+
+def law_6(request):
+    return render(request, 'assistance/law/law-6.html')
+
+def int_1(request):
+    return render(request, 'assistance/int/int-1.html')
+
+def int_2(request):
+    return render(request, 'assistance/int/int-2.html')
+
+def int_3(request):
+    return render(request, 'assistance/int/int-3.html')
+
+def int_4(request):
+    return render(request, 'assistance/int/int-4.html')
+
+def int_5(request):
+    return render(request, 'assistance/int/int-5.html')
+
+def int_6(request):
+    return render(request, 'assistance/int/int-6.html')
+
+def int_7(request):
+    return render(request, 'assistance/int/int-7.html')
